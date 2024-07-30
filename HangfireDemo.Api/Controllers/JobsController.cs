230046840c156cbc1d7a4de705eee79ebd352186
Daniel Lerps <lerps.daniel.ext@siemens.com>
2024-07-30 13:21:07 +0000

    Scaffolding

---
+++ HangfireDemo.Api/Controllers/JobsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HangfireDemo.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class JobsController : ControllerBase
+{
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateJob()
+    {
+        //var jobId = backgroundJobClient.Enqueue(() => Console.WriteLine("Hello Hangfire"));
+        return Accepted("11");
+    }
+}
